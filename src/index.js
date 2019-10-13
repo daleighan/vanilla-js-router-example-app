73c749e0c85ec97914afbc34bbbcae99dc2bc827
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 20:57:19 -0700

    Now serving the bundle when it is requested

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import VanillaJSRouter from 'vanilla-js-router';
 
 const router = new VanillaJSRouter('anchor', {
-  '/': '<div>Home</div>'
-  '/about': '<div>About</div>'
+  '/': '<div>Home</div>',
+  '/about': '<div>About</div>',
   '/projects': '<div>Projects</div>',
   '/contact': '<div>Contact</div>',
 });
