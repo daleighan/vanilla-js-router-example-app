0e54f01995445c9417084feeabe0559a1b232e33
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 22:44:27 -0700

    Now using the header and footer options for navigation

--- src/index.js
+++ src/index.js
@@ -8,5 +8,15 @@ const router = new VanillaJSRouter(
     '/projects/': '<div>Projects</div>',
     '/contact/': '<div>Contact</div>',
   },
-  process.env.NODE_ENV !== 'production',
+  {
+    debug: process.env.NODE_ENV !== 'production',
+    errorHTML: '<div>404 Route Not Found</div>',
+    header: ` 
+      <a class="router-link" href="/">Home</a>
+      <a class="router-link" href="/projects/">Projects</a>
+      <a class="router-link" href="/about/">About Me</a>
+      <a class="router-link" href="/contact/">Reach Out</a>
+    `,
+    footer: 'footer text',
+  },
 );
