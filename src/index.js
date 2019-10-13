304807d2e134261fd6df9b9dafab8dfa562c9caa
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 20:31:58 -0700

    Adding initial config with webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import VanillaJSRouter from 'vanilla-js-router';
+
+const router = new VanillaJSRouter('anchor', {});
+
+
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'static'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
