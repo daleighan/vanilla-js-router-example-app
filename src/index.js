77e4dfd9ee721e1284bb2265fbfb54d12ec227bc
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 20:39:51 -0700

    Added an object with some routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import VanillaJSRouter from 'vanilla-js-router';
 
-const router = new VanillaJSRouter('anchor', {});
+const router = new VanillaJSRouter('anchor', {
+  '/': '<div>Home</div>'
+  '/about': '<div>About</div>'
+  '/projects': '<div>Projects</div>',
+  '/contact': '<div>Contact</div>',
+});
 
 
