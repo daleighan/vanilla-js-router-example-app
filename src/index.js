cfed4dec00a1d3ebdb392136c86dcb42c0930be9
David Alexander Leigh <daleighan@gmail.com>
2019-10-13 00:12:33 -0700

    Using pre-generated html elements

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import VanillaJSRouter from 'vanilla-js-router';
 
+const home = document.createElement('div');
+home.innerHTML = 'Home';
+const about = document.createElement('div');
+about.innerHTML = 'About';
+const projects = document.createElement('div');
+projects.innerHTML = 'projects';
+const contact = document.createElement('div');
+contact.innerHTML = 'Contact';
+
 const router = new VanillaJSRouter(
   'anchor',
   {
-    '/': '<div>Home</div>',
-    '/about/': '<div>About</div>',
-    '/projects/': '<div>Projects</div>',
-    '/contact/': '<div>Contact</div>',
+    '/': home,
+    '/about': about,
+    '/projects': projects,
+    '/contact': contact,
   },
   {
     debug: process.env.NODE_ENV !== 'production',
     errorHTML: '<div>404 Route Not Found</div>',
-    header: ` 
-      <a class="router-link" href="/">Home</a>
-      <a class="router-link" href="/projects/">Projects</a>
-      <a class="router-link" href="/about/">About Me</a>
-      <a class="router-link" href="/contact/">Reach Out</a>
-    `,
+    header: `<a class="router-link" href="/">Home</a><a class="router-link" href="/projects">Projects</a><a class="router-link" href="/about">About Me</a><a class="router-link" href="/contact">Reach Out</a>`,
     footer: 'footer text',
   },
 );
