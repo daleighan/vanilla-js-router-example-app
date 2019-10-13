36f41aa3ded005fb0e0f53e574840625d842a5ff
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 21:23:40 -0700

    All routes should now return the required assets from the static folder

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'static')));
 
-app.get('*bundle.js', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, `static/bundle.js`))
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'static/index.html'));
 });
+
 
 app.listen(process.env.PORT || 4200, () => console.log('express is listening'));
