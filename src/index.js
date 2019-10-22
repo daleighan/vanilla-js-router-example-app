40d0362507168bf9124230a92aac16555c52c9f7
David Alexander Leigh <daleighan@gmail.com>
2019-10-21 21:54:27 -0700

    Using prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import VanillaJSRouter from 'vanilla-js-router';
+import VanillaJSRouter from 'vanilla-js-router'
 
-const home = document.createElement('div');
-home.innerHTML = 'Home';
-const about = document.createElement('div');
-about.innerHTML = 'About';
-const projects = document.createElement('div');
-projects.innerHTML = 'projects';
-const contact = document.createElement('div');
-contact.innerHTML = 'Contact';
+const home = document.createElement('div')
+home.innerHTML = 'Home'
+const about = document.createElement('div')
+about.innerHTML = 'About'
+const projects = document.createElement('div')
+projects.innerHTML = 'Projects'
+function contact() {
+  const contact = document.createElement('div')
+  contact.innerHTML = 'Contact'
+  return contact
+}
 
 const router = new VanillaJSRouter(
   'anchor',
@@ -19,8 +22,8 @@ const router = new VanillaJSRouter(
   },
   {
     debug: process.env.NODE_ENV !== 'production',
-    errorHTML: '<div>404 Route Not Found</div>',
+    errorHTML: '<div>Route Not Found</div>',
     header: `<a class="router-link" href="/">Home</a><a class="router-link" href="/projects">Projects</a><a class="router-link" href="/about">About Me</a><a class="router-link" href="/contact">Reach Out</a>`,
     footer: 'footer text',
   },
-);
+)
