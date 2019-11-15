ffc3aaab93304b69d0e6b7b6e694245ebff850a1
David Alexander Leigh <daleighan@gmail.com>
2019-11-14 19:25:13 -0800

    Added some commenting

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 import VanillaJSRouter from '@daleighan/vanilla-js-router';
 
-function Home() {
-  const home = document.createElement('div');
-  home.textContent = 'home';
-  return home;
-}
+// This is the first way to make a component to be used on a route
+// using just a string
+const Home = '<div>Home</div>';
 
+// This is a second way using a function that returns a string
 function About() {
-  const about = document.createElement('div');
-  about.textContent = 'about';
-  return about;
+  return `
+    <div>
+      About
+    </div>
+  `;
 }
 
-function Project({ params }) {
-  const project = document.createElement('div');
-  project.textContent = `project ${params.id}`;
-  return project;
-}
-
+// This is the third way using a function that returns a DOM element
 function Contact() {
   const contact = document.createElement('div');
   contact.textContent = 'contact';
   return contact;
 }
 
+// This component has the params of the route passed into it
+function Project({ params }) {
+  const project = document.createElement('div');
+  project.textContent = `project ${params.id}`;
+  return project;
+}
+
 const router = new VanillaJSRouter(
+  // The router will be attached to an element with id="anchor"
   'anchor',
   {
     '/': Home,
     '/about': About,
-    '/projects/:id': Project,
     '/contact': Contact,
+    '/projects/:id': Project,
   },
+  // All the items passed into the options object below are optional
   {
     debug: process.env.NODE_ENV !== 'production',
     errorHTML: '<div>Route Not Found</div>',
